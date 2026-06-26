858eddb8e19035d845f8581eb22ef39859770c85
sungandi86-max <sungandi86@gmail.com>
2026-06-26 09:31:09 +0900

    Add Vercel cache

--- api/portal.js
+++ api/portal.js
@@ -15,6 +15,7 @@ function jsonError(res, status, message, debug) {
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate=600");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
